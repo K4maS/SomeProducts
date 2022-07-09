b72d98cb41c83dd7b02efbd060a42248241c9efa
K4maS <107948849+K4maS@users.noreply.github.com>
2022-07-09 22:11:01 +0500

    Добавлены доп кнопки в  слайдер в резюме

--- Резюме/resume.js
+++ Резюме/resume.js
@@ -1,18 +1,67 @@
-var galery = ["chsultanov.jpg", "my_photo.jpg", "list_image_mint20.png"];
-            img.src = galery[0];
-            var x = 0;
-           
-            forward_btn.onclick = function(){
-            x ++;
-            if (x >= galery.length){
-                x = 0;   
-            }
-            img.src = galery[x];
-            }
-            back_btn.onclick =function(){
-            x --;  
-            if (x< 0){
-            x = galery.length-1;
-            }
-            img.src = galery[x];
-            }
+var galery = ["cert-html.jpg", "cert-sql.png", "test-school-cert.png", 'my_photo.jpg', "chsultanov.jpg"];
+img.src = galery[0];
+var x = 0;
+
+forward_btn.onclick = function () {
+    x++;
+    if (x >= galery.length) {
+        x = 0;
+    }
+    img.src = galery[x];
+}
+back_btn.onclick = function () {
+    x--;
+    if (x < 0) {
+        x = galery.length - 1;
+    }
+    img.src = galery[x];
+}
+
+var kontent_val = galery.length
+num = 0;
+for (let i = 0; i < galery.length; i++) {
+    var newbtn = document.createElement("button");
+    dot.appendChild(newbtn);
+    newbtn.value = i;
+    switch (i) {
+        case 0:
+            newbtn.setAttribute('style', 'left: 45%');
+            break;
+        case 1:
+            newbtn.setAttribute('style', 'left: 48%');
+            break;
+        case 2:
+            newbtn.setAttribute('style','left: 51%');
+            break;
+        case 3:
+            newbtn.setAttribute('style','left: 54%');
+            break;
+        case 4:
+            newbtn.setAttribute('style','left: 57%');
+            break;
+        case 5:
+            newbtn.setAttribute('style','left: 60%');
+            break;
+        case 6:
+            newbtn.setAttribute('style','left: 63%');
+            break;
+        case 7:
+            newbtn.setAttribute('style','left: 66%');
+            break;
+        case 8:
+            newbtn.setAttribute('style','left: 69%');
+            break;
+        case 9:
+            newbtn.setAttribute('style','left: 72%');
+            break;
+        case 10:
+            newbtn.setAttribute('style','left: 75%');
+            break;
+    }
+
+    newbtn.setAttribute("onclick", 'dotclick(' + i + ')');
+}
+function dotclick(i) { 
+    img.src = galery[i];
+    document.getElementById('"'+i+'"').style.style.display="none";
+ }
