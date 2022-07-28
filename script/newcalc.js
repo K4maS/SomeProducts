a821f5e0dd00992c6b8e426818994462916a2ee1
K4maS <107948849+K4maS@users.noreply.github.com>
2022-07-29 00:51:41 +0500

    'Добавлены исправленяи в калькулятор'

--- script/newcalc.js
+++ script/newcalc.js
@@ -3,16 +3,22 @@ var value2 = Array();
 var num = 0;
 var have_dot = false;
 var action_active = false;
+var first_in = true;
+
+
+
+
 function add_value(x){
     //строит из нажатых клавиш чилсло переменной
-    if(value1 == 0){
+    if(first_in){
         value1 = String(x);
-        }
-    else {
+        first_in = false;
+       }
+     else {
         value1+= String(x);
         num+=1;
-        value1 = Number(value1);
-        }
+        
+         }
     if(value2.length != 0  ){
         pool_calc.innerHTML = value2[0] + value2[1] + value1 ;
        
@@ -21,17 +27,27 @@ function add_value(x){
     else{pool_calc.innerHTML = value1; 
         document.getElementById('value_input').value = value2[0] + value2[1] + value1;}
     }
+
+
+
+
+
 function posneg() {
-    //перевод чилса в позитивный или негативный вид
+    //перевод чилса в позитивный или негативный видs
     value1 = value1 * -1;
+    
     if(value2.length != 0  ){
         pool_calc.innerHTML = value2[0] + value2[1] + value1 ;
     }
     else{pool_calc.innerHTML = value1; 
         document.getElementById('value_input').value = value2[0] + value2[1] + value1;}
     }
+
+
+
+
 function add_dot(){
-    //
+    //добавляет знак дроби
     if(have_dot == false){
     value1 = String(value1)+'.';
     if(value2.length != 0  ){
@@ -40,8 +56,12 @@ function add_dot(){
     else{pool_calc.innerHTML = value1; 
         document.getElementById('value_input').value = value2[0] + value2[1] + value1;}
     have_dot = true;
-    }}
+    }
+}
     
+
+
+
 function clear_all(){
     //отчищаются переменные
     value1 = 0;
@@ -49,35 +69,43 @@ function clear_all(){
     value2  = [];
     have_dot = false;
     action_active = false;
+    first_in = true;
     pool_calc.innerHTML = value1+' '+value2;
-    }
+}
+
+
+
+
 function action(sumbol){
     //после нажатия на оператор, система передает значение 
     //первой переменной на другую переменную и знак оператора в список value2, value1 отчищается
-    if(action_active == false) {    
-        value2[0] = value1;
+    
+    if(action_active == false) { 
+     
+        value2[0] = Number(value1);
         value2[1] = sumbol;
         value1 = 0;
         num = 0;
         have_dot = false;
         action_active = true;
+        first_in = true;
         pool_calc.innerHTML = value2[0] + value2[1] ;
         return value2 
     }
+     
 }
+
 
 
 
 function result(){
     //выведение результата
     var total;
-    
+    value1=Number(value1);
     //if( isEmpty( value2[0] ) ){value2[0] = 0; value2[1]=' ';}
-    if(typeof value1 === 'string' && typeof value2[0] === 'string'){
-        value1 = Number(value1);
-        value2[0] = Number(value2[0]);
-     }
+  
     if(action_active){
+      
         if(value2[1]=='+'){
         total = value2[0]+value1;
         }
@@ -94,9 +122,11 @@ function result(){
     
         }
         else{ total = value1 ;}
-
         pool_calc.innerHTML = value2[0] + value2[1] + value1 + '='+ total ;
         value2[0] = total;
-        }
-    action_active = false;
+        action_active = false;
+        first_in = true;
+        have_dot = false;
     }
+    
+}
